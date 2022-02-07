f07d092d4f5c22ccc9f5e08db075d3cc9ac846b5
mikesch803 <mikesch803@gmail.com>
2022-02-07 16:06:43 +0530

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,18 +1,16 @@
-var containerModal = document.querySelector(".modal");
-var btnModalOpen = document.querySelector(".btn-modal-open");
-var btnModalClose = document.querySelector(".btn-modal-close");
+const containerModal = document.querySelector(".modal");
+const btnModalOpen = document.querySelector(".btn-modal-open");
+const btnModalClose = document.querySelector(".btn-modal-close");
 
 containerModal.style.display = "none";
-console.log(containerModal)
-console.log(btnModalClose)
-console.log(btnModalOpen)
-var openModelHandler = () => {
+
+const openModelHandler = () => {
     containerModal.style.display = "flex";
     btnModalOpen.style.display = "none";
     console.log(true)
 }
 
-var closeModelHandler = () => {
+const closeModelHandler = () => {
     containerModal.style.display = "none";
     btnModalOpen.style.display = "block";
     console.log(true)
